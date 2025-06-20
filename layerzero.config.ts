5d02af239fa9186f71bb2bb382bff52145a8249c
saitama2009 <saitama@1combo.io>
2025-06-21 01:44:15 +0800

    config dev contracts

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -10,15 +10,20 @@ const bscContract: OmniPointHardhat = {
     contractName: 'CycleNetworkToken',
 }
 
-const beraContract: OmniPointHardhat = {
-    eid: EndpointId.BERA_V2_MAINNET,
+const ethContract: OmniPointHardhat = {
+    eid: EndpointId.ETHEREUM_V2_MAINNET,
     contractName: 'CycleNetworkOFT',
 }
 
-const arbitrumContract: OmniPointHardhat = {
-    eid: EndpointId.ARBITRUM_V2_MAINNET,
-    contractName: 'CycleNetworkOFT',
-}
+// const beraContract: OmniPointHardhat = {
+//     eid: EndpointId.BERA_V2_MAINNET,
+//     contractName: 'CycleNetworkOFT',
+// }
+
+// const arbitrumContract: OmniPointHardhat = {
+//     eid: EndpointId.ARBITRUM_V2_MAINNET,
+//     contractName: 'CycleNetworkOFT',
+// }
 
 // To connect all the above chains to each other, we need the following pathways:
 // Optimism <-> Avalanche
@@ -28,6 +33,7 @@ const arbitrumContract: OmniPointHardhat = {
 // For this example's simplicity, we will use the same enforced options values for sending to all chains
 // For production, you should ensure `gas` is set to the correct value through profiling the gas usage of calling OFT._lzReceive(...) on the destination chain
 // To learn more, read https://docs.layerzero.network/v2/concepts/applications/oapp-standard#execution-options-and-enforced-settings
+// https://docs.layerzero.network/v2/developers/evm/create-lz-oapp/configuring-pathways#adding-enforcedoptionss
 const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
     {
         msgType: 1,
@@ -42,32 +48,32 @@ const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
 const pathways: TwoWayConfig[] = [
     [
         bscContract, // Chain A contract
-        beraContract, // Chain B contract
+        ethContract, // Chain B contract
         [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
+        [20, 15], // [A to B confirmations, B to A confirmations]
         [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain B enforcedOptions, Chain A enforcedOptions
     ],
-    [
-        bscContract, // Chain A contract
-        arbitrumContract, // Chain C contract
-        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
-        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain A enforcedOptions
-    ],
-    [
-        beraContract, // Chain B contract
-        arbitrumContract, // Chain C contract
-        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
-        [1, 1], // [A to B confirmations, B to A confirmations]
-        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain B enforcedOptions
-    ],
+    // [
+    //     bscContract, // Chain A contract
+    //     arbitrumContract, // Chain C contract
+    //     [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+    //     [1, 1], // [A to B confirmations, B to A confirmations]
+    //     [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain A enforcedOptions
+    // ],
+    // [
+    //     beraContract, // Chain B contract
+    //     arbitrumContract, // Chain C contract
+    //     [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ]
+    //     [1, 1], // [A to B confirmations, B to A confirmations]
+    //     [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain B enforcedOptions
+    // ],
 ]
 
 export default async function () {
     // Generate the connections config based on the pathways
     const connections = await generateConnectionsConfig(pathways)
     return {
-        contracts: [{ contract: bscContract }, { contract: beraContract }, { contract: arbitrumContract }],
+        contracts: [{ contract: bscContract }, { contract: ethContract }],
         connections,
     }
 }
