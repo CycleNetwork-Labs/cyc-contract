3bf6522ed542441c0aa20dd25e742a78e6f808c5
saitama2009 <saitama@1combo.io>
2025-06-24 17:33:10 +0800

    safe bridge script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,6 +14,7 @@ import { HardhatUserConfig, HttpNetworkAccountsUserConfig } from 'hardhat/types'
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
 import './tasks/transfer'
+import './tasks/transfer_safe'
 
 // Set your preferred authentication method
 //
