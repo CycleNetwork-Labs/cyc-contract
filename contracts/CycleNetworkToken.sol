03ee2d793f534ffeff754f907256a7a0141bda53
saitama2009 <saitama@1combo.io>
2025-07-31 15:43:27 +0800

    update contracts

--- contracts/CycleNetworkToken.sol
+++ contracts/CycleNetworkToken.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract WhitelistedTimeLockedERC20 is ERC20, Ownable {
-    // Timestamp after which transfers are allowed for non-whitelisted users
+
+contract CycleNetworkToken is ERC20, Ownable {
     uint256 public transferAllowedTimestamp;
     uint256 internal ETA;
-    // Mapping to track whitelisted addresses
     mapping(address => bool) public whitelist;
+
     event NewTransferAllowedTimestamp(uint256 newTimestamp);
     event WhitelistAdded(address user);
     event WhitelistRemoved(address user);
+
     constructor(
         string memory name,
         string memory symbol,
@@ -22,32 +23,30 @@ contract WhitelistedTimeLockedERC20 is ERC20, Ownable {
         whitelist[msg.sender] = true;
         _mint(msg.sender, initialSupply);
     }
-    // Function to update the transfer allowed timestamp
+
     function setTransferAllowedTimestamp(uint256 newTimestamp) external onlyOwner {
         if (transferAllowedTimestamp > block.timestamp && ETA == 0) {
-            // any value is accepted
             transferAllowedTimestamp = newTimestamp;
         } else {
             if (ETA == 0) {
                 ETA = transferAllowedTimestamp + 1 days;
             }
-            // can not be later than ETA
             require(newTimestamp <= ETA, "ETA!");
             transferAllowedTimestamp = newTimestamp;
         }
         emit NewTransferAllowedTimestamp(newTimestamp);
     }
-    // Function to add an address to the whitelist
+
     function addToWhitelist(address user) external onlyOwner {
         whitelist[user] = true;
         emit WhitelistAdded(user);
     }
-    // Function to remove an address from the whitelist
+
     function removeFromWhitelist(address user) external onlyOwner {
         whitelist[user] = false;
         emit WhitelistRemoved(user);
     }
-    // Override the _beforeTokenTransfer hook to enforce time lock and whitelist
+
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
         require(block.timestamp >= transferAllowedTimestamp || whitelist[from] || whitelist[to], "not allowed");
         super._beforeTokenTransfer(from, to, amount);
